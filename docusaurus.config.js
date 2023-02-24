a6cae8dd2bbd336fcfa248f5ba14189e35499238
he <MY_NAME@example.com>
2023-02-24 23:03:01 +0800

    更改主要信息

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Web Dev',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://dev.yiyideer.com/',
+  title: 'All about Tofu',
+  tagline: 'Tofu is a great food',
+  url: 'https://tofu.yiyideer.com/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Tofu', // Usually your GitHub org/user name.
+  projectName: 'Tofu', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -27,10 +27,10 @@ const config = {
     locales: ['en'],
     localeConfigs: {
       en: {
-        htmlLang: 'en-GB',
+        htmlLang: 'en-US',
       },
       // You can omit a locale (e.g. fr) if you don't need to override the defaults
-    }
+    },
   },
 
   presets: [
@@ -40,20 +40,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/', // Serve the docs at the site's root
-          /* other docs plugin options */
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -61,78 +49,15 @@ const config = {
       }),
     ],
   ],
-  plugins: [
-    [
-      require.resolve('@cmfcmf/docusaurus-search-local'),
-      {
-        // whether to index docs pages
-        indexDocs: true,
-
-        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
-        // 0 disables this feature.
-        // 1 indexes the direct parent category in the sidebar of a doc page
-        // 2 indexes up to two nested parent categories of a doc page
-        // 3...
-        //
-        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
-        indexDocSidebarParentCategories: 0,
-
-        // whether to index blog pages
-        indexBlog: false,
-
-        // whether to index static pages
-        // /404.html is never indexed
-        indexPages: false,
-
-        // language of your documentation, see next section
-        language: 'en',
-
-        // setting this to "none" will prevent the default CSS to be included. The default CSS
-        // comes from autocomplete-theme-classic, which you can read more about here:
-        // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
-        style: undefined,
-
-        // lunr.js-specific settings
-        lunr: {
-          // When indexing your documents, their content is split into "tokens".
-          // Text entered into the search box is also tokenized.
-          // This setting configures the separator used to determine where to split the text into tokens.
-          // By default, it splits the text at whitespace and dashes.
-          //
-          // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
-          tokenizerSeparator: /[\s\-]+/,
-          // https://lunrjs.com/guides/customising.html#similarity-tuning
-          //
-          // This parameter controls the importance given to the length of a document and its fields. This
-          // value must be between 0 and 1, and by default it has a value of 0.75. Reducing this value
-          // reduces the effect of different length documents on a term’s importance to that document.
-          b: 0.75,
-          // This controls how quickly the boost given by a common word reaches saturation. Increasing it
-          // will slow down the rate of saturation and lower values result in quicker saturation. The
-          // default value is 1.2. If the collection of documents being indexed have high occurrences
-          // of words that are not covered by a stop word filter, these words can quickly dominate any
-          // similarity calculation. In these cases, this value can be reduced to get more balanced results.
-          k1: 1.2,
-          // By default, we rank pages where the search term appears in the title higher than pages where
-          // the search term appears in just the text. This is done by "boosting" title matches with a
-          // higher value than content matches. The concrete boosting behavior can be controlled by changing
-          // the following settings.
-          titleBoost: 5,
-          contentBoost: 1,
-          parentCategoriesBoost: 2, // Only used when indexDocSidebarParentCategories > 0
-        },
-      },
-    ],
-  ],
-  
+  plugins: [],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Web Dev',
+        title: 'Tofu',
         logo: {
-          alt: 'Web Dev Logo',
+          alt: 'Tofu Logo',
           src: 'img/seal-logo.png',
         },
         items: [
@@ -142,18 +67,11 @@ const config = {
             position: 'left',
             label: 'Welcome',
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   href: 'https://github.com/facebook/docusaurus',
-          //   label: 'GitHub',
-          //   position: 'right',
-          // },
         ],
       },
       footer: {
         style: 'dark',
-        links: [
-        ],
+        links: [],
         copyright: `Copyright © ${new Date().getFullYear()}`,
       },
       prism: {
